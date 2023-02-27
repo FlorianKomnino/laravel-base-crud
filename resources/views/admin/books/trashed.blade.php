243564716e95f300f7ba10df4abba953db208437
albertobaggio96 <albertobaggio96@gmail.com>
2023-02-27 16:25:29 +0100

    create tash blade

---
+++ resources/views/admin/books/trashed.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container py-5">
+    <div class="row">
+        <div class="col-12">
+            @if (session('message'))
+                <div class="alert alert-{{ session('message_class') }} mb-3">
+                    {{ session('message') }}
+                </div>
+            @endif
+        </div>
+        <div class="col-12">
+            <a href="{{route('admin.books.create')}}" class="btn btn-success">
+                Create new book
+            </a>
+        </div>
+        <div class="col-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">title</th>
+                        <th scope="col">author</th>
+
+                        <th scope="col">actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($trashedBooks as $book)
+                        <tr>
+                            <th scope="row">{{ $book->id }}</th>
+                            <td>{{ $book->title }}</td>
+                            <td>{{ $book->author }}</td>
+
+                            <td>
+                                <div class="actionButtons d-flex">
+                                    <form action="{{route('admin.forceDelete', $book->id)}}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-danger">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                    
+                </tbody>
+            </table>
+            {{-- {{ $books->links() }} --}}
+        </div>
+    </div>
+</div>
+@endsection
