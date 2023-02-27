e44f7af0998158aa63201e78e76c82de53454678
RenatoFringuello <renatofringuello@hotmail.it>
2023-02-27 11:23:04 +0100

    add Book model

---
+++ app/Models/Book.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'isbn-13',// nella validation mettiamo una limitazione sui numero dei caratteri : esattamente 13
+        /**
+         * attenzione che il title e series non sono unique e lo slug dovrà avere un contatore (es. zanna-bianca-2')
+         * si può creare lo slug
+         * fare una query (where 'slug' = $slugAppenaCreato)
+         * se la query ci restituisce qualcosa
+         * $slugAppenaCreato .= count($slugTrovatiNellaQuery)
+         */
+        // 'slug', 
+        'title',
+        'series',
+        'author',
+        'publisher',
+        'publication_date',
+        'plot',
+        'genre',
+        'cover_image',
+    ];
+}
