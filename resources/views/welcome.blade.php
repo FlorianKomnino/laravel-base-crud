dc986677c07cbb4cee30ca9d0e47054c35d10391
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-27 16:30:54 +0100

    fix margin into books-preview section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
 <section id="books-preview" class="p-5">
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 mb-3">
                 <h3>
                     Our best sellers:
                 </h3>
