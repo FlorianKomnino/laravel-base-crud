e648854aed7c48f82b0308220c2d5c36c1b519b2
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-27 16:27:57 +0100

    added our best sellers section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,4 +15,42 @@
         </div>
     </div>
 </section>
+<section id="books-preview" class="p-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>
+                    Our best sellers:
+                </h3>
+            </div>
+        </div>
+        <div class="row d-flex">
+            @for ($i=0; $i < 3; $i++)
+            <div class="col-4">
+                <div class="card text-start">
+                    <div class="card-header">
+                        <p class="card-text text-center">
+                            isbn
+                        </p>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            title
+                        </h4>
+                        <h6>
+                            author
+                        </h6>
+                        <p class="card-text">
+                            cover_image
+                        </p>
+                        <p class="card-text">
+                            genre
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endfor
+        </div>
+    </div>
+</section>
 @endsection
