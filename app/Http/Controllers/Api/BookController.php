e614550d7256f4177c4cfefd26684858b5696a65
FlorianKomnino <florian.komnino@outlook.it>
2023-03-07 17:19:45 +0100

    created api managment (controller, routes) and api_token column

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -14,7 +15,12 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+        $books = Book::paginate(25);
+
+        return response()->json([
+            'success' => true,
+            'result' => $books,
+        ]);
     }
 
     /**
